a1a8d3fc67c3d6eedf57d71ea7902eb919887ff3
sexjpg <37231405+sexjpg@users.noreply.github.com>
2021-03-16 20:11:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,9 +15,11 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default #passwall的依赖
+sed -i '$a src-git lua-maxminddb https://github.com/jerrykuku/lua-maxminddb' feeds.conf.default #vssr的依赖
+
 
 # HelloWorld ,要配合Argon主题https://github.com/jerrykuku/luci-app-vssr
-sed -i '$a src-git helloworld https://github.com/jerrykuku/luci-app-vssr' feeds.conf.default
+# sed -i '$a src-git helloworld https://github.com/jerrykuku/luci-app-vssr' feeds.conf.default
 # sed -i '$a src-git helloworld https://github.com/jerrykuku/luci-theme-argon' feeds.conf.default
